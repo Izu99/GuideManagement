127fea1b8231e6faeb2ae8993f2c13275fe95dda
Sachin Harshitha <39330996+thelllmike@users.noreply.github.com>
2023-04-18 00:58:45 +0530

    fix guide male

---
+++ src/Components/FemaleGuidetableColum.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+import axios from "axios";
+
+
+
+class GGtableRow extends Component {
+    constructor(props) {
+        super(props);
+        this.delete = this.delete.bind(this);
+    }
+    delete(){
+        axios.get('http://localhost:8090/femaleGuide/delete/'+this.props.obj._id)
+            .then(this.setState({redirect: true}))
+            .catch(err => console.log(err))
+        //this.props.history.push('/index');
+        alert(" Successfully Deleted....")
+        window.location.replace('/GuideTableView');
+    }
+    render() {
+        return (
+           <tr>
+               
+               <th>
+                   {this.props.obj.fullName}
+               </th>
+               <th>
+                   {this.props.obj.location}
+               </th>
+          
+              
+           </tr>
+        );
+    }
+}
+
+export default GGtableRow;
